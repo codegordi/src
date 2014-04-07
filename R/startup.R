7e95ba05a5995bd18a139df2988f18c8d015449d
Christina G <cgutierrez@sailthru.com>
2014-04-07 17:03:02 -0400

    src/ initial commit (public)

---
+++ R/startup.R
@@ -0,0 +1,22 @@
+######################################
+#
+# startup.R
+#
+# cgutierrez - my startup sourcer
+#
+#####################################
+
+library(ggplot2)
+library(plyr)
+library(dplyr)
+library(reshape2)
+library(rjson)
+library(lubridate)
+#library(RColorBrewer)
+library(assertthat)
+library(stringr)
+library(scales)
+
+home.dir = "/home/cgutierrez/"
+src.dir = "/home/cgutierrez/src/R/"
+
